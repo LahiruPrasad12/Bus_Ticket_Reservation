4a6f8f4a38b546986e387d4e49733701a1673f24
99hash63 <thkottegoda@gmail.com>
2021-05-07 20:53:00 +0530

    designed layout fragment_add_routes

--- app/src/main/java/com/example/busticketreservation/Admin/AddRoutesFragment.java
+++ app/src/main/java/com/example/busticketreservation/Admin/AddRoutesFragment.java
@@ -16,5 +16,7 @@ public class AddRoutesFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_add_routes, container, false);
+
+
     }
 }
