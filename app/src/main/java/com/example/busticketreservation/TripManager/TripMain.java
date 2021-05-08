46509123d5770f1c68e24ed3ad2b53528c8d61bb
pamodaravihansa <pamoda.ravihansa@gmail.com>
2021-05-08 18:00:09 +0530

    convert fragments to activities

--- app/src/main/java/com/example/busticketreservation/TripManager/TripMain.java
+++ app/src/main/java/com/example/busticketreservation/TripManager/TripMain.java
@@ -1,80 +1,68 @@
-package com.example.busticketreservation.TripManager;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
-
-import android.os.Bundle;
-import android.view.MenuItem;
-
-import com.example.busticketreservation.R;
-import com.google.android.material.navigation.NavigationView;
-
-
-public class TripMain extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
-
-    private DrawerLayout drawer;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_trip_main);
-
-//      using the toolbar as the action bar
-        Toolbar toolbar = findViewById(R.id.pamo_toolbar);
-        setSupportActionBar(toolbar);
-
-//      getting the drawer layout
-        drawer = findViewById(R.id.hash_drawer_layout);
-        NavigationView navigationView = findViewById(R.id.pamo_nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-
-//      get the menu button in the top left corner
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
-                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-
-        //start all routes fragment immediately when main activity is started
-        if(savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.pamo_fragment_container,
-                    new ViewTripsFragment()).commit();
-            navigationView.setCheckedItem(R.id.nav_view_trips);
-        }
-    }
-
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-
-            case R.id.nav_view_trips:
-                getSupportFragmentManager().beginTransaction().replace(R.id.pamo_fragment_container,
-                        new ViewTripsFragment()).commit();
-                break;
-
-            case R.id.nav_add_trips:
-                getSupportFragmentManager().beginTransaction().replace(R.id.pamo_fragment_container,
-                        new AddTripsFragment()).commit();
-                break;
-        }
-
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
-
-    @Override
-//    close navigation bar when back button is clicked
-    public void onBackPressed() {
-        if(drawer.isDrawerOpen(GravityCompat.START)){
-            drawer.closeDrawer(GravityCompat.START);
-        }else{
-            super.onBackPressed();
-        }
-    }
-
-
-
-}
+//package com.example.busticketreservation.TripManager;
+//
+//import androidx.annotation.NonNull;
+//import androidx.appcompat.app.ActionBarDrawerToggle;
+//import androidx.appcompat.app.AppCompatActivity;
+//import androidx.appcompat.widget.Toolbar;
+//import androidx.core.view.GravityCompat;
+//import androidx.drawerlayout.widget.DrawerLayout;
+//
+//import android.app.Activity;
+//import android.content.Intent;
+//import android.os.Bundle;
+//import android.view.MenuItem;
+//import android.view.View;
+//
+//import com.example.busticketreservation.R;
+//import com.google.android.material.navigation.NavigationView;
+//
+//
+//public class TripMain extends AppCompatActivity {
+//
+//    private DrawerLayout drawer;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_view_trips);
+//
+////      using the toolbar as the action bar
+//        Toolbar toolbar = findViewById(R.id.pamo_toolbar);
+//        setSupportActionBar(toolbar);
+//
+////      get the menu button in the top left corner
+//        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+//                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+//        drawer.addDrawerListener(toggle);
+//        toggle.syncState();
+//
+//    }
+//
+//    public void ClickViewTrips(View view){
+//        //Redirect activity to add trips
+////        redirectActivity(this,AddTrips.class);
+//        startActivity(new Intent(getApplicationContext(),AddTrips.class));
+//    }
+//
+//    public void ClickAddTrips(View view){
+//        //Redirect activity to select routes
+//        //redirectActivity(this,SelectRoutesTm.class);
+//    }
+//
+//    private static void redirectActivity(Activity activity, Class aClass) {
+//        //Initialize intent
+//        Intent intent = new Intent(activity,aClass);
+//        //Set flag
+//        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//        //Start activity
+//        activity.startActivity(intent);
+//
+//    }
+//
+//
+//}
+//
+//
+//
+//
+//
