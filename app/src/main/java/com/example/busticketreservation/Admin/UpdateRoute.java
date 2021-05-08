0f73c3cfc740a1af8030b5cafb6a6d133c7454a2
99hash63 <thkottegoda@gmail.com>
2021-05-08 21:33:10 +0530

    completed navigation view of UpdateRoute activity

--- app/src/main/java/com/example/busticketreservation/Admin/UpdateRoute.java
+++ app/src/main/java/com/example/busticketreservation/Admin/UpdateRoute.java
@@ -1,15 +1,24 @@
 package com.example.busticketreservation.Admin;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
 import com.example.busticketreservation.R;
+import com.google.android.material.navigation.NavigationView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.EditText;
 
-public class UpdateRoute extends AppCompatActivity {
+public class UpdateRoute extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     EditText routeNOE, fromE, toE, priceE;
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,5 +52,57 @@ public class UpdateRoute extends AppCompatActivity {
         fromE.setText(route.getFrom());
         toE.setText(route.getTo());
         priceE.setText(String.valueOf(route.getPrice()));
+
+
+        //      using the toolbar as the action bar
+        Toolbar toolbar = findViewById(R.id.hash_toolbar);
+        setSupportActionBar(toolbar);
+
+//        getting the drawer layout
+        drawer = findViewById(R.id.hash_drawer_layout);
+
+        //        listen to click events of the navigation view
+        NavigationView navigationView = findViewById(R.id.hash_nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+//        get the menu button in the top left corner
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    @Override
+//    passing menu item to setNavigationItemSelectedListener
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.nav_all_routes:
+                startActivity(new Intent(getApplicationContext(), AllRoutes.class));
+                break;
+            case R.id.nav_add_routes:
+                startActivity(new Intent(getApplicationContext(), AddRoutes.class));
+                break;
+            case R.id.nav_all_users:
+                startActivity(new Intent(getApplicationContext(), AllUsers.class));
+                break;
+            case R.id.nav_add_users:
+                startActivity(new Intent(getApplicationContext(), AddUsers.class));
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+//    close navigation bar when back button is clicked
+    public void onBackPressed() {
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }else{
+            super.onBackPressed();
+        }
+
     }
 }
