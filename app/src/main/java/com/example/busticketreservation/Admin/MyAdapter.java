da1ff9ca9d2416d8eb70406af9cfa5f57fb99112
99hash63 <thkottegoda@gmail.com>
2021-05-08 13:06:32 +0530

    created all routes recycle view to retrieve and display data

---
+++ app/src/main/java/com/example/busticketreservation/Admin/MyAdapter.java
@@ -0,0 +1,56 @@
+package com.example.busticketreservation.Admin;
+
+import com.example.busticketreservation.R;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+
+    Context context;
+    ArrayList<Routes> list;
+
+    public MyAdapter(Context context, ArrayList<Routes> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(context).inflate(R.layout.item, parent, false);
+        return new MyViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+        Routes routes =list.get(position);
+        holder.routeNo.setText(routes.getRouteNo());
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder{
+
+        TextView routeNo;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            routeNo = itemView.findViewById(R.id.tvRouteNo);
+
+        }
+    }
+}
