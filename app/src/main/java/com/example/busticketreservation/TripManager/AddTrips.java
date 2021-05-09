d3fe505d624e9db5648741ea849f0e2847b2f1cc
pamodaravihansa <pamoda.ravihansa@gmail.com>
2021-05-09 19:15:53 +0530

    Updated layouts

--- app/src/main/java/com/example/busticketreservation/TripManager/AddTrips.java
+++ app/src/main/java/com/example/busticketreservation/TripManager/AddTrips.java
@@ -2,14 +2,30 @@ package com.example.busticketreservation.TripManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import com.example.busticketreservation.R;
 
 public class AddTrips extends AppCompatActivity {
+
+    Button btcancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_trips);
+
+        /*navigate to select route */
+        btcancel = findViewById(R.id.btncancel);
+        btcancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),SelectRoutesTm.class));
+            }
+        });
+
     }
 }
