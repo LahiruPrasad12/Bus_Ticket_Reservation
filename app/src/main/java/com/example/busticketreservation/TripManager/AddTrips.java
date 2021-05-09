292293a6f6bd12ae59eba143b48d4aa2ee6a2963
pamodaravihansa <pamoda.ravihansa@gmail.com>
2021-05-09 21:41:45 +0530

    crud operations

--- app/src/main/java/com/example/busticketreservation/TripManager/AddTrips.java
+++ app/src/main/java/com/example/busticketreservation/TripManager/AddTrips.java
@@ -4,19 +4,36 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.busticketreservation.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class AddTrips extends AppCompatActivity {
 
-    Button btcancel;
+    EditText trip_id, date , dep_time, ar_time;
+    Button btcancel,btnaddtrip;
+    Trip trp;
+    DatabaseReference dbref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_trips);
+
+        trip_id = findViewById(R.id.addtripid);
+        date = findViewById(R.id.add_date);
+        dep_time = findViewById(R.id.txt_dep_time);
+        ar_time = findViewById(R.id.txt_ar_time);
+
+        btnaddtrip = findViewById(R.id.button_add_trip);
+
+
 
         /*navigate to select route */
         btcancel = findViewById(R.id.btncancel);
@@ -27,5 +44,58 @@ public class AddTrips extends AppCompatActivity {
             }
         });
 
+        /*add data to the database */
+
+        btnaddtrip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dbref = FirebaseDatabase.getInstance().getReference().child("Trip");
+
+                try {
+                    if (TextUtils.isEmpty(trip_id.getText().toString()))
+                        Toast.makeText(getApplicationContext(),"Empty ID",Toast.LENGTH_SHORT).show();
+                    else if (TextUtils.isEmpty(date.getText().toString()))
+                        Toast.makeText(getApplicationContext(),"Empty Date",Toast.LENGTH_SHORT).show();
+                    else if (TextUtils.isEmpty(dep_time.getText().toString()))
+                        Toast.makeText(getApplicationContext(),"Empty Depature Time",Toast.LENGTH_SHORT).show();
+                    else if (TextUtils.isEmpty(ar_time.getText().toString()))
+                        Toast.makeText(getApplicationContext(),"Empty Arrival Time",Toast.LENGTH_SHORT).show();
+                    else {
+
+                        trp.setTrip_id(trip_id.getText().toString().trim());
+                        trp.setDate(date.getText().toString().trim());
+                        trp.setDep_time(dep_time.getText().toString().trim());
+                        trp.setAr_time(ar_time.getText().toString().trim());
+                        dbref.child("Trip1").setValue(trp);
+
+                        Toast.makeText(getApplicationContext(),"Succesfully inserted",Toast.LENGTH_SHORT).show();
+                        clearControls();
+
+                    }
+                }
+                catch (NumberFormatException nfe){
+                    Toast.makeText(getApplicationContext(),"Please Enter a Number In here",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+
     }
+
+
+
+
+
+
+    /*to clear text fields*/
+
+    private void clearControls(){
+        trip_id.setText("");
+        date.setText("");
+        dep_time.setText("");
+        ar_time.setText("");
+    }
+
+
 }
---
+++ app/src/main/java/com/example/busticketreservation/TripManager/Trip.java
@@ -0,0 +1,43 @@
+package com.example.busticketreservation.TripManager;
+
+public class Trip {
+    private String trip_id;
+    private String date;
+    private String dep_time;
+    private String ar_time;
+
+    public Trip() {
+    }
+
+    public String getTrip_id() {
+        return trip_id;
+    }
+
+    public void setTrip_id(String trip_id) {
+        this.trip_id = trip_id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getDep_time() {
+        return dep_time;
+    }
+
+    public void setDep_time(String dep_time) {
+        this.dep_time = dep_time;
+    }
+
+    public String getAr_time() {
+        return ar_time;
+    }
+
+    public void setAr_time(String ar_time) {
+        this.ar_time = ar_time;
+    }
+}
